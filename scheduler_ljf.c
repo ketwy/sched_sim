7fdc75df16603b8be5bcec81c48c8cdd93297032
José Marques <jose.marques15@hotmail.com>
2023-06-25 13:48:01 -0300

    feat: desenvolvimento dos algoritmos

--- scheduler_ljf.c
+++ scheduler_ljf.c
@@ -18,8 +18,52 @@ extern int MAX_TIME;
 
 struct proc * scheduler(struct proc * current)
 {
-    struct proc * selected; 
+    struct proc * selected;
 
-    return NULL;
+    if (current != NULL)
+    {
+        switch (current->state)
+        {
+            case READY:
+                enqueue(ready, current);
+                count_ready_in(current);
+                break;
+            case BLOCKED:
+                enqueue(blocked, current);
+                count_blocked_in(current);
+                break;
+            case FINISHED:
+                enqueue(finished, current);
+                count_finished_in(current);
+                break;
+            default:
+                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        }
+    }
+
+    if (isempty(ready))
+    {
+        return NULL;
+    }
+
+    // Estratégia IJF: seleciona o processo com o maior tempo restante de execução
+    selected = ready->head;
+    struct proc *current_process = ready->head->next;
+
+    while (current_process != NULL)
+    {
+        if (current_process->remaining_time > selected->remaining_time)
+        {
+            selected = current_process;
+        }
+        current_process = current_process->next;
+    }
+
+    // Remove o processo selecionado da fila de aptos
+    dequeue_bypid(ready, selected->pid);
+
+    count_ready_out(selected);
+    selected->state = RUNNING;
+    return selected;
 }
 
