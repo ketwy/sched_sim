d726c0810d3cf6edb1e22c3a2b505bdb7c199236
José Marques <jose.marques15@hotmail.com>
2023-06-25 16:57:44 -0300

    fix: correção do algoritmo

--- scheduler_prio_static.c
+++ scheduler_prio_static.c
@@ -7,40 +7,63 @@
 #include "utils.h" // possui funções uteis 
 
 // Utilizando as variáveis globais definidas no 'main'
-extern struct queue *ready;    // fila de aptos
-extern struct queue *ready2;   // segunda fila de aptos
-extern struct queue *blocked;  // fila de bloqueados
-extern struct queue *finished; // fila de finalizados
+extern struct queue * ready;    // fila de aptos
+extern struct queue * ready2;   // segunda fila de aptos
+extern struct queue * blocked;  // fila de bloqueados
+extern struct queue * finished; // fila de finalizados
 // NOTE: essa fila de finalizados é utilizada apenas para
 // as estatisticas finais
 
 // variavel global que indica o tempo maximo que um processo pode executar ao todo
 extern int MAX_TIME;
 
-struct proc *scheduler(struct proc *current)
+struct proc * scheduler(struct proc * current)
 {
     struct proc *selected;
 
-    // Verifica se a fila 1 (ready) possui processos
-    if (!isempty(ready))
+    if (current != NULL)
     {
-        // Seleciona um processo da fila 1 (ready) usando a estratégia FIFO (80% de probabilidade)
-        if (random() <= 0.8)
+        switch (current->state)
         {
-            selected = dequeue(ready); // Remove o processo selecionado da fila 1 (ready)
-            return selected;
-        }
-    }
-    // Verifica se a fila 2 (ready2) possui processos
-    if (!isempty(ready2))
-    {
-        // Seleciona um processo da fila 2 (ready2) usando a estratégia FIFO (20% de probabilidade)
-        if (random() <= 0.2)
-        {
-            selected = dequeue(ready2); // Remove o processo selecionado da fila 2 (ready2)
-            return selected;
+            case READY:
+                if (current->queue == 1) {
+                    enqueue(ready, current);
+                } else {
+                    enqueue(ready2, current);
+                }
+                count_ready_in(current);
+                break;
+            case BLOCKED:
+                enqueue(blocked, current);
+                count_blocked_in(current);
+                break;
+            case FINISHED:
+                enqueue(finished, current);
+                count_finished_in(current);
+                break;
+            default:
+                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
         }
     }
 
-    return NULL; // Retorna NULL se nenhuma fila tiver processos
+    if (isempty(ready) && isempty(ready2))
+    {
+        return NULL;
+    }
+
+    // Verifica se a fila 1 (ready) possui processos
+    if (!isempty(ready) && make_rand(100) <= 80)
+    {
+        selected = dequeue(ready); // Remove o processo selecionado da fila 1 (ready)
+    }
+    // Verifica se a fila 2 (ready2) possui processos
+    else if (!isempty(ready2))
+    {
+        // Seleciona um processo da fila 2 (ready2) usando a estratégia FIFO (20% de probabilidade)
+        selected = dequeue(ready2); // Remove o processo selecionado da fila 2 (ready2)
+    }
+
+    count_ready_out(selected);
+    selected->state = RUNNING;
+    return selected;
 }
