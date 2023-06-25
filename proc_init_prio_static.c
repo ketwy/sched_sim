76c608884b43c8762cf5bc16ac8d0e385e23ea14
José Marques <jose.marques15@hotmail.com>
2023-06-25 13:47:34 -0300

    feat: desenvolvimento dos algoritmos

--- proc_init_prio_static.c
+++ proc_init_prio_static.c
@@ -11,17 +11,20 @@
 extern int NPROC;
 
 // Utilizando as variáveis globais das filas definidas no 'main'
-extern struct queue * ready;    // fila de aptos
-extern struct queue * ready2;   // segunda fila de aptos
-extern struct queue * blocked;  // fila de bloqueados
-extern struct queue * finished; // fila de finalizados
+extern struct queue *ready;    // fila de aptos
+extern struct queue *ready2;   // segunda fila de aptos
+extern struct queue *blocked;  // fila de bloqueados
+extern struct queue *finished; // fila de finalizados
+
+// Tempo máximo de execução total de um processo
+extern int MAX_TIME;
 
 // Inicializa os 'NPROC' processos informados
 void proc_init()
 {
     struct proc *p;
 
-    for (int i = 0; i < NPROC; i++) 
+    for (int i = 0; i < NPROC; i++)
     {
         // criando um novo processo
         p = createproc();
@@ -29,11 +32,18 @@ void proc_init()
         // cria a thread do processo
         p->tid = createproc_thread(p);
 
-        // adicionando-o na fila de aptos
-        enqueue(ready, p);
-
-        // contabilizando a entrada do processo na fila de aptos
-        count_ready_in(p);
+        // Verifica a prioridade do processo com base no tempo restante
+        if (p->remaining_time <= 0.2 * MAX_TIME)
+        {
+            p->queue = 1;
+            // Adiciona o processo na primeira fila de aptos
+            enqueue(ready, p);
+        }
+        else
+        {
+            p->queue = 2;
+            // Adiciona o processo na segunda fila de aptos
+            enqueue(ready2, p);
+        }
     }
 }
-
