723ed47e5fc17e6c0ea95f67c7cc047c9ea57c02
ketwy <ketllyazevedo14@gmail.com>
2023-06-25 22:28:18 -0300

    fazendo pequenas alterações nos arquivos de prioridade

--- scheduler_prio_dynamic.c
+++ scheduler_prio_dynamic.c
@@ -1,63 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "queue.h"
+#include "proc.h"
+#include "stats.h"
+#include "utils.h"
 
-#include "queue.h" // contem funções uteis para filas
-#include "proc.h"  // possui as funções dos processos
-#include "stats.h" // possui as funções de estatisticas 
-#include "utils.h" // possui funções uteis 
+// Variáveis globais definidas no 'main'
+extern struct queue *ready;    // fila de processos aptos
+extern struct queue *ready2;   // segunda fila de processos aptos
+extern struct queue *blocked;  // fila de processos bloqueados
+extern struct queue *finished; // fila de processos finalizados
+// OBS: A fila 'finished' é usada apenas para estatísticas finais
 
-// Utilizando as variáveis globais definidas no 'main'
-extern struct queue *ready;    // fila de aptos
-extern struct queue *ready2;   // segunda fila de aptos
-extern struct queue *blocked;  // fila de bloqueados
-extern struct queue *finished; // fila de finalizados
-// NOTE: essa fila de finalizados é utilizada apenas para
-// as estatisticas finais
-
-// variavel global que indica o tempo maximo que um processo pode executar ao todo
-extern int MAX_TIME;
+extern int MAX_TIME; // tempo máximo que um processo pode executar ao todo
 
 struct proc *scheduler(struct proc *current)
 {
-    struct proc *selected;
-
     if (current != NULL)
     {
         switch (current->state)
         {
-            case READY:
-                enqueue(ready2, current);
-                count_ready_in(current);
-                break;
-            case BLOCKED:
-                enqueue(blocked, current);
-                count_blocked_in(current);
-                break;
-            case FINISHED:
-                enqueue(finished, current);
-                count_finished_in(current);
-                break;
-            default:
-                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        case READY:
+            enqueue(ready2, current); // Coloca o processo atual na segunda fila de aptos (ready2)
+            count_ready_in(current);  // Atualiza as estatísticas de processos aptos
+            break;
+        case BLOCKED:
+            enqueue(blocked, current); // Coloca o processo atual na fila de bloqueados (blocked)
+            count_blocked_in(current); // Atualiza as estatísticas de processos bloqueados
+            break;
+        case FINISHED:
+            enqueue(finished, current); // Coloca o processo atual na fila de finalizados (finished)
+            count_finished_in(current); // Atualiza as estatísticas de processos finalizados
+            break;
+        default:
+            printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+            // Caso o estado do processo atual não seja reconhecido, exibe uma mensagem de erro
         }
     }
 
     if (isempty(ready) && isempty(ready2))
     {
-        return NULL;
+        return NULL; // Se ambas as filas de aptos estiverem vazias, retorna NULL indicando que não há mais processos a serem executados
     }
 
-    // Selecionar um processo para execução
+    struct proc *selected;
+
     if (!isempty(ready) && (isempty(ready2) || make_rand(100) <= 80))
     {
-        selected = dequeue(ready); // Selecionar um processo da Fila 1 (ready) com 80% de probabilidade
+        selected = dequeue(ready); // Seleciona um processo da primeira fila de aptos (ready) com 80% de probabilidade
     }
     else
     {
-        selected = dequeue(ready2); // Selecionar um processo da Fila 2 (ready2) com 20% de probabilidade
+        selected = dequeue(ready2); // Seleciona um processo da segunda fila de aptos (ready2) com 20% de probabilidade
     }
 
-    count_ready_out(selected);
-    selected->state = RUNNING;
-    return selected;
+    count_ready_out(selected); // Atualiza as estatísticas de processos aptos
+    selected->state = RUNNING; // Define o estado do processo selecionado como RUNNING (em execução)
+    return selected;           // Retorna o processo selecionado para ser executado
 }
--- scheduler_prio_static.c
+++ scheduler_prio_static.c
@@ -1,69 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "queue.h"
+#include "proc.h"
+#include "stats.h"
+#include "utils.h"
 
-#include "queue.h" // contem funções uteis para filas
-#include "proc.h"  // possui as funções dos processos
-#include "stats.h" // possui as funções de estatisticas 
-#include "utils.h" // possui funções uteis 
+extern struct queue *ready;    // fila de aptos
+extern struct queue *ready2;   // segunda fila de aptos
+extern struct queue *blocked;  // fila de bloqueados
+extern struct queue *finished; // fila de finalizados
 
-// Utilizando as variáveis globais definidas no 'main'
-extern struct queue * ready;    // fila de aptos
-extern struct queue * ready2;   // segunda fila de aptos
-extern struct queue * blocked;  // fila de bloqueados
-extern struct queue * finished; // fila de finalizados
-// NOTE: essa fila de finalizados é utilizada apenas para
-// as estatisticas finais
+extern int MAX_TIME; // variável global que indica o tempo máximo que um processo pode executar ao todo
 
-// variavel global que indica o tempo maximo que um processo pode executar ao todo
-extern int MAX_TIME;
-
-struct proc * scheduler(struct proc * current)
+struct proc *scheduler(struct proc *current)
 {
-    struct proc *selected;
-
     if (current != NULL)
     {
         switch (current->state)
         {
-            case READY:
-                if (current->queue == 1) {
-                    enqueue(ready, current);
-                } else {
-                    enqueue(ready2, current);
-                }
-                count_ready_in(current);
-                break;
-            case BLOCKED:
-                enqueue(blocked, current);
-                count_blocked_in(current);
-                break;
-            case FINISHED:
-                enqueue(finished, current);
-                count_finished_in(current);
-                break;
-            default:
-                printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
+        case READY:
+            // Adiciona o processo atual à fila correspondente (ready ou ready2)
+            if (current->queue == 1)
+            {
+                enqueue(ready, current);
+            }
+            else
+            {
+                enqueue(ready2, current);
+            }
+            count_ready_in(current); // Atualiza estatísticas de entrada de processos aptos
+            break;
+        case BLOCKED:
+            enqueue(blocked, current); // Adiciona o processo atual à fila de bloqueados
+            count_blocked_in(current); // Atualiza estatísticas de entrada de processos bloqueados
+            break;
+        case FINISHED:
+            enqueue(finished, current); // Adiciona o processo atual à fila de finalizados
+            count_finished_in(current); // Atualiza estatísticas de entrada de processos finalizados
+            break;
+        default:
+            printf("@@ ERRO no estado de saída do processo %d\n", current->pid);
         }
     }
 
+    // Verifica se ambas as filas de aptos estão vazias
     if (isempty(ready) && isempty(ready2))
     {
-        return NULL;
+        return NULL; // Retorna NULL se não houver mais processos aptos para executar
     }
 
-    // Verifica se a fila 1 (ready) possui processos
+    struct proc *selected = NULL;
+
+    // Seleciona o próximo processo a ser escalonado
     if (!isempty(ready) && make_rand(100) <= 80)
     {
-        selected = dequeue(ready); // Remove o processo selecionado da fila 1 (ready)
+        selected = dequeue(ready); // Remove o processo selecionado da fila ready
     }
-    // Verifica se a fila 2 (ready2) possui processos
     else if (!isempty(ready2))
     {
-        // Seleciona um processo da fila 2 (ready2) usando a estratégia FIFO (20% de probabilidade)
-        selected = dequeue(ready2); // Remove o processo selecionado da fila 2 (ready2)
+        selected = dequeue(ready2); // Remove o processo selecionado da fila ready2
     }
 
-    count_ready_out(selected);
-    selected->state = RUNNING;
-    return selected;
+    if (selected != NULL)
+    {
+        count_ready_out(selected); // Atualiza estatísticas de saída de processos aptos
+        selected->state = RUNNING; // Define o estado do processo selecionado como RUNNING
+    }
+
+    return selected; // Retorna o processo selecionado para ser escalonado
 }
